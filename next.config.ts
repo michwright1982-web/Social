854e733fd1e38e0a69ad1c37b9badcae6c80fc9e
vinith <vinith.thayalan@gmail.com>
2026-06-03 12:10:05 +0530

    fb post

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  serverExternalPackages: [],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
 };
 
 export default nextConfig;
